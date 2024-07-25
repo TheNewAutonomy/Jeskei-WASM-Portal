61c826f3a4307e7e673ca1463011df516cda1333
$ whoami <darren@ourfamily.io>
2024-07-25 18:59:48 +0100

    Added support for MetaMask

---
+++ Jeskei.Wasm/Pages/Subscriptions.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace JeskeiPortal.Client.Pages
+{
+    public partial class Subscriptions : IDisposable
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        private void NavigationManager_LocationChanged(object? sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
+        {
+        }
+
+        void IDisposable.Dispose()
+        {
+            // Unsubscribe from the event when our component is disposed
+            NavigationManager.LocationChanged -= NavigationManager_LocationChanged;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Jeskei Blazor portal
+This projects provides a portal for interacting with the Jeskei plpatform.
+
+Activities supported include:
++ View video content on the Jeskei platform
++ Create new content
++ Create digital studios for collaboration and brand building
+
+## Getting Started
+Connect the MetaMask wallet to the Jeskei platform to get started.
