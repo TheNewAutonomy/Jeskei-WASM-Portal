011d1326142357d32c9eac554c2cdeb7718db075
$ whoami <darren@ourfamily.io>
2024-08-01 11:01:44 +0100

    Completed IPFS upload, add skeleton code to register ownership onchain

--- Jeskei.Wasm/wwwroot/js/uploadHelper.js
+++ Jeskei.Wasm/wwwroot/js/uploadHelper.js
@@ -1,12 +1,26 @@
 ﻿// wwwroot/js/uploadHelper.js
 window.uploadHelper = {
     getFile: function (inputElement) {
+        if (inputElement.files.length === 0) {
+            return null;
+        }
+
+        const file = inputElement.files[0];
+
+        return {
+            name: file.name,
+            size: file.size,
+            type: file.type,
+            lastModified: file.lastModified
+        };
+    },
+    readFileAsBase64: function (inputElement) {
         return new Promise((resolve, reject) => {
-            if (inputElement.files.length === 0) {
-                resolve(null);
-            } else {
-                resolve(inputElement.files[0]);
-            }
+            const file = inputElement.files[0];
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result.split(',')[1]); // Get Base64 string without prefix
+            reader.onerror = error => reject(error);
+            reader.readAsDataURL(file);
         });
     }
 };
